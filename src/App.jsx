45ceb23d13851280d6eb57faaf8869dd619aea39
Kyoku <henji0905@gmail.com>
2025-08-06 21:10:18 +0500

    features sec made + adaptive for tablets

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import Header from './components/Header'
 
 import intro from '/assets/images/intro/intro.png'
 
+import redRect from '/assets/images/features/rect1.svg'
+import blueRect from '/assets/images/features/rect2.svg'
+import yellowRect from '/assets/images/features/rect3.svg'
+import goToPage from '/assets/images/features/go-to-page.svg'
+import allInAll from '/assets/images/features/all-in-all.svg'
+import pcs from '/assets/images/features/pcs.svg'
+import phones from '/assets/images/features/phones.svg'
+
 function App() {
 
   return (
@@ -16,8 +24,36 @@ function App() {
             <p className="intro-text">Smartphones and Laptops <br/> with Quality Guarantee</p>
             <button className='intro-button'>Catalog</button>
           </div>
-          <img src={intro} alt="Intro-image" className="intro-image" />
+          <div className='intro-image'>
+            <img src={intro} alt="Intro-image"/>
+          </div>
         </div>
+      </section>
+
+      <section className='features-section'>
+        <div className='feature'>
+          <img src={redRect} alt="" />
+          <h2>The laptop that <span className='green-words'>does <br/> it all</span></h2>
+          <img src={goToPage} alt="" className='feature-go-to-img'/>
+          <img src={pcs} alt="" className='feature-img'/>
+        </div>
+        <div className='feature'>
+          <img src={blueRect} alt=""/>
+          <h2>The <span className='green-words'>best phones</span> of recent years</h2>
+          <h3>2025</h3>
+          <img src={goToPage} alt="" className='feature-go-to-img' />
+          <img src={phones} alt=""  className='feature-img'/>
+        </div>
+        <div className='feature'>
+          <img src={yellowRect} alt="" />
+          <h2><span className='green-words'>All</span> the best - in one place</h2>
+          <img src={goToPage} alt="" className='feature-go-to-img' />
+          <img src={allInAll} alt="" className='feature-img'/>
+        </div>
+      </section>
+      <section className='products-section'>
+      </section>
+      <section className='collections-section'>
       </section>
     </>
   )
