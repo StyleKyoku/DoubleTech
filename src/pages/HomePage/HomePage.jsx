5791d8a0a7ddcdbf2ef93f2a2a1f7ecf362200c7
StyleKyoku <henji0905@gmail.com>
2025-10-20 01:09:05 +0100

    Finished refactoring of HomePage.scss to HomePage.module.scss

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,62 +1,90 @@
-import { Link } from 'react-router-dom'
+import styles from "./HomePage.module.scss";
 
-import "../../styles/HomePage.scss"
+import Card from "../../components/Card/Card.jsx";
 
-import Card from '../../components/Card/Card.jsx'
-
-
-import intro from '/assets/images/intro/intro.png'
-import redRect from '/assets/images/features/rect1.svg'
-import blueRect from '/assets/images/features/rect2.svg'
-import yellowRect from '/assets/images/features/rect3.svg'
-import goToPage from '/assets/images/features/go-to-page.svg'
-import allInAll from '/assets/images/features/all-in-all.svg'
-import pcs from '/assets/images/features/pcs.svg'
-import phones from '/assets/images/features/phones.svg'
+import intro from "/assets/images/intro/intro.png";
+import redRect from "/assets/images/features/rect1.svg";
+import blueRect from "/assets/images/features/rect2.svg";
+import yellowRect from "/assets/images/features/rect3.svg";
+import goToPage from "/assets/images/features/go-to-page.svg";
+import allInAll from "/assets/images/features/all-in-all.svg";
+import pcs from "/assets/images/features/pcs.svg";
+import phones from "/assets/images/features/phones.svg";
 
 export default function HomePage() {
   return (
     <>
-      <section className='intro-section'>
-        <div className="intro-content">
-          <div className="intro-text-wrapper">
-            <h1 className="intro-title">Your Tech — Your Rules</h1>
-            <h2 className="intro-text">Smartphones and Laptops <br /> with Quality Guarantee</h2>
-
-            <button className='intro-button'>Catalog</button>
+      <section className={styles["intro-section"]}>
+        <div className={styles["intro-content"]}>
+          <div className={styles["intro-text-wrapper"]}>
+            <h1 className={styles["intro-title"]}>Your Tech — Your Rules</h1>
+            <h2 className={styles["intro-text"]}>
+              Smartphones and Laptops <br /> with Quality Guarantee
+            </h2>
+            <button className={styles["intro-button"]}>Catalog</button>
           </div>
-          <div className='intro-image'>
+          <div className={styles["intro-image"]}>
             <img src={intro} alt="Intro-image" />
           </div>
         </div>
       </section>
-
-      <section className='features-section'>
-        <div className='feature'>
-          <img src={redRect} alt="" />
-          <h2>The laptop that <span className='green-words'>does <br /> it all</span></h2>
-          <img src={goToPage} alt="" className='feature-go-to-img' />
-          <img src={pcs} alt="" className='feature-img' />
+      <section className={styles["features-section"]}>
+        <div className={styles["feature"]}>
+          <img src={redRect} alt="red bg" />
+          <h2>
+            The laptop that
+            <span className={styles["green-words"]}>
+              does <br /> it all
+            </span>
+          </h2>
+          <img
+            src={goToPage}
+            alt="go to page img"
+            className={styles["feature-go-to-img"]}
+          />
+          <img src={pcs} alt="pcs img" className={styles["feature-img"]} />
         </div>
-        <div className='feature'>
-          <img src={blueRect} alt="" />
-          <h2>The <span className='green-words'>best phones</span> of recent years</h2>
+        <div className={styles["feature"]}>
+          <img src={blueRect} alt="blue bg" />
+          <h2>
+            The <span className={styles["green-words"]}>best phones</span> of
+            recent years
+          </h2>
           <h3>2025</h3>
-          <img src={goToPage} alt="" className='feature-go-to-img' />
-          <img src={phones} alt="" className='feature-img' />
+          <img
+            src={goToPage}
+            alt="go to page img"
+            className={styles["feature-go-to-img"]}
+          />
+          <img
+            src={phones}
+            alt="phones img"
+            className={styles["feature-img"]}
+          />
         </div>
-        <div className='feature'>
-          <img src={yellowRect} alt="" />
-          <h2><span className='green-words'>All</span> the best - in one place</h2>
-          <img src={goToPage} alt="" className='feature-go-to-img' />
-          <img src={allInAll} alt="" className='feature-img' />
+        <div className={styles["feature"]}>
+          <img src={yellowRect} alt="yellow bg" />
+          <h2>
+            <span className={styles["green-words"]}>All</span> the best - in one
+            place
+          </h2>
+          <img
+            src={goToPage}
+            alt="go to page img"
+            className={styles["feature-go-to-img"]}
+          />
+          <img
+            src={allInAll}
+            alt="all in all img"
+            className={styles["feature-img"]}
+          />
         </div>
       </section>
 
-      <section className='products-section'>
-        <div className="products-wrapper">
-          <h3 className='product-title'>Best Sellers</h3>
-          <div className='products-list'>
+      <section className={styles["products-section"]}>
+        <div className={styles["products-wrapper"]}>
+          <h3 className={styles["product-title"]}>Best Sellers</h3>
+          <div className={styles["products-list"]}>
             <Card
               id="654321"
               title='iPhone 16 Pro Max, 256 GB, 8 GB RAM, A18 Pro, 6.9" OLED, 48 MP, USB-C, Titanium, iOS 18'
@@ -79,9 +107,9 @@ export default function HomePage() {
             />
           </div>
         </div>
-        <div className="products-wrapper">
-          <h3 className='product-title'>Sales</h3>
-          <div className='products-list'>
+        <div className={styles["products-wrapper"]}>
+          <h3 className={styles["product-title"]}>Sales</h3>
+          <div className={styles["products-list"]}>
             <Card
               id="123456"
               title='iPhone 16 Pro Max, 256 GB, 8 GB RAM, A18 Pro, 6.9" OLED, 48 MP, USB-C, Titanium, iOS 18'
@@ -142,58 +170,92 @@ export default function HomePage() {
               onSale={false}
               originalPrice=""
             />
-            
           </div>
         </div>
       </section>
 
-      <section className='collections-section'>
-        <h3 className='collections-title'>Collections</h3>
-        <div className="collections-wrapper">
-          <div className="collection">
+      <section className={styles["collections-section"]}>
+        <h3 className={styles["collections-title"]}>Collections</h3>
+        <div className={styles["collections-wrapper"]}>
+          <div className={styles["collection"]}>
             <img
-              src={import.meta.env.BASE_URL + "/assets/images/collections/rect1.svg"}
+              src={
+                import.meta.env.BASE_URL +
+                "/assets/images/collections/rect1.svg"
+              }
               alt="School Collection"
-              className="collection-bg" />
+              className={styles["collection-bg"]}
+            />
             <img
-              src={import.meta.env.BASE_URL + "/assets/images/collections/school.svg"}
+              src={
+                import.meta.env.BASE_URL +
+                "/assets/images/collections/school.svg"
+              }
               alt="Go to school collection"
-              className='go-to-col' />
+              className={styles["go-to-col"]}
+            />
             <img
-              src={import.meta.env.BASE_URL + "/assets/images/collections/school-img.png"}
+              src={
+                import.meta.env.BASE_URL +
+                "/assets/images/collections/school-img.png"
+              }
               alt="School img"
-              className="collection-img" />
+              className={styles["collection-img"]}
+            />
           </div>
-          <div className="collection">
+          <div className={styles["collection"]}>
             <img
-              src={import.meta.env.BASE_URL + "/assets/images/collections/rect2.svg"}
+              src={
+                import.meta.env.BASE_URL +
+                "/assets/images/collections/rect2.svg"
+              }
               alt="Work Collection"
-              className="collection-bg" />
+              className={styles["collection-bg"]}
+            />
             <img
-              src={import.meta.env.BASE_URL + "/assets/images/collections/work.svg"}
+              src={
+                import.meta.env.BASE_URL + "/assets/images/collections/work.svg"
+              }
               alt="Go to work collection"
-              className='go-to-col' />
+              className={styles["go-to-col"]}
+            />
             <img
-              src={import.meta.env.BASE_URL + "/assets/images/collections/work-img.png"}
+              src={
+                import.meta.env.BASE_URL +
+                "/assets/images/collections/work-img.png"
+              }
               alt="Work img"
-              className="collection-img" />
+              className={styles["collection-img"]}
+            />
           </div>
-          <div className="collection">
+          <div className={styles["collection"]}>
             <img
-              src={import.meta.env.BASE_URL + "/assets/images/collections/rect3.svg"}
+              src={
+                import.meta.env.BASE_URL +
+                "/assets/images/collections/rect3.svg"
+              }
               alt="Gaming Collection"
-              className="collection-bg" />
+              className={styles["collection-bg"]}
+            />
             <img
-              src={import.meta.env.BASE_URL + "/assets/images/collections/gaming.svg"}
+              src={
+                import.meta.env.BASE_URL +
+                "/assets/images/collections/gaming.svg"
+              }
               alt="Go to gaming collection"
-              className='go-to-col' />
+              className={styles["go-to-col"]}
+            />
             <img
-              src={import.meta.env.BASE_URL + "/assets/images/collections/gaming-img.png"}
+              src={
+                import.meta.env.BASE_URL +
+                "/assets/images/collections/gaming-img.png"
+              }
               alt="Gaming img"
-              className="collection-img" />
+              className={styles["collection-img"]}
+            />
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
