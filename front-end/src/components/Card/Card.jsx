3b05464e06cfee00a928c1a290a95099c0f33305
StyleKyoku <henji0905@gmail.com>
2025-11-01 22:04:45 +0000

    Made a test fetch for back-end api

--- front-end/src/components/Card/Card.jsx
+++ front-end/src/components/Card/Card.jsx
@@ -1,19 +1,39 @@
 import React from "react";
-import styles from './Card.module.scss'
+import styles from "./Card.module.scss";
 
-import sale from '/assets/images/products/sales.svg'
-import cart from '/assets/images/products/cart.svg'
+import sale from "/assets/images/products/sales.svg";
+import cart from "/assets/images/products/cart.svg";
 
-const Card = ({ id, title, price, imageUrl, inBasket = false, category = "everything", onSale, originalPrice="" }) => {
+const Card = ({
+  id,
+  title,
+  price,
+  imageUrl,
+  inBasket = false,
+  category = "everything",
+  onSale,
+  originalPrice = "",
+}) => {
   const [isInBasket, setIsInBasket] = React.useState(Boolean(inBasket));
 
+  const [cardInfo, setCardInfo] = React.useState({
+    title: "",
+    description: "",
+  });
+
+  React.useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/card")
+      .then((res) => res.json())
+      .then((data) => setCardInfo(data))
+      .catch((err) => console.error("Error: ", err));
+  }, []);
+
   const handleBasket = () => {
-    setIsInBasket(prev => !prev);
+    setIsInBasket((prev) => !prev);
   };
 
   const handleBuy = () => {
     setIsInBasket(true);
-    
   };
 
   return (
@@ -21,24 +41,44 @@ const Card = ({ id, title, price, imageUrl, inBasket = false, category = "everyt
       <div className={styles["card-image-div"]}>
         <button className={styles["like-button"]} onClick={handleBasket}>
           <img
-            src={isInBasket
-              ? import.meta.env.BASE_URL + "/assets/images/products/isLiked.svg"
-              : import.meta.env.BASE_URL + "/assets/images/products/notLiked.svg"}
+            src={
+              isInBasket
+                ? import.meta.env.BASE_URL +
+                "/assets/images/products/isLiked.svg"
+                : import.meta.env.BASE_URL +
+                "/assets/images/products/notLiked.svg"
+            }
             alt="like icon"
             className={styles["like-icon"]}
           />
         </button>
-        <img src={import.meta.env.BASE_URL + imageUrl} alt={title} className={styles["card-image"]} />
+        <img
+          src={import.meta.env.BASE_URL + imageUrl}
+          alt={title}
+          className={styles["card-image"]}
+        />
       </div>
       <div className={styles["card-info"]}>
         <div className={styles["card-price-container"]}>
-          {onSale ? <img src={sale} alt="Sales img" className={styles["card-sales-image"]} /> : null}
-          <p className={`${styles["card-price"]} ${onSale ? styles["on-sale"] : ""}`}>${price}</p>
-          {onSale ? <p className={styles["card-price-original"]}>${originalPrice}</p> : null}
+          {onSale ? (
+            <img
+              src={sale}
+              alt="Sales img"
+              className={styles["card-sales-image"]}
+            />
+          ) : null}
+          <p
+            className={`${styles["card-price"]} ${onSale ? styles["on-sale"] : ""}`}
+          >
+            ${price}
+          </p>
+          {onSale ? (
+            <p className={styles["card-price-original"]}>${originalPrice}</p>
+          ) : null}
         </div>
         <p className={styles["card-category"]}>for {category}</p>
       </div>
-      <small className={styles["card-title"]}>{title}</small>
+      <small className={styles["card-title"]}>{cardInfo.title}</small>
       <button className={styles["card-buy"]} onClick={handleBuy}>
         <img src={cart} alt="Cart icon" className={styles["cart-icon"]} />
         Buy
